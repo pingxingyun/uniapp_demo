82adeb29282afb4a62d7ce770b1eae1f1b7aaa64
fcx_workmac <fcx@pingxingyun.com>
2022-08-04 15:49:02 +0800

    add ios webview setup

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ const Config = {
   // WebSocket: "wss://cloudlark.pingxingyun.com:8182/",
   Host: "https://cloudlark.pingxingyun.com:8180/",
   WebSocket: "wss://cloudlark.pingxingyun.com:8180/",
+  // Host: "http://192.168.0.55:8181/",
+  // WebSocket: "wss://192.168.0.55:8181/",
   ShareTitle: "LarkSR",
 }
 
--- pages/webclient/webclient.vue
+++ pages/webclient/webclient.vue
@@ -1,16 +1,18 @@
 <template>
-<view class="container">
-  <web-view :src="url"></web-view>
+<view>
+  <web-view v-if="!isIOS" :src="url"></web-view>
 </view>
 </template>
 
 <script>
 	import Fetch from '../../utils/fetch';
 	import Config from '../../config.js';
+	
 	export default {
 		data() {
 			return {
 				url: "",
+				isIOS: false,
 			};
 		},
 		onLoad(option) {
@@ -19,7 +21,26 @@
 			console.log("iframe url ", iframeurl);
 			
 			// this.url = iframeurl; */
-			this.url = option.url;
+			
+			if (uni.getSystemInfoSync().platform == "ios") {
+				this.isIOS = true;
+				var wv = plus.webview.create("","custom-webview",{
+					// plusrequire:"none", //禁止远程网页使用plus的API，有些使用mui制作的网页可能会监听plus.key，造成关闭页面混乱，可以通过这种方式禁止
+				   // 'uni-app': 'none', //不加载uni-app渲染层框架，避免样式冲突
+					top:uni.getSystemInfoSync().statusBarHeight+44, //放置在titleNView下方。如果还想在webview上方加个地址栏的什么的，可以继续降低TOP值
+					kernel: "UIWebview",
+				});
+				
+				wv.loadURL(option.url);
+				// wv.loadURL("https://www.baidu.com");
+				
+				var currentWebview = this.$scope.$getAppWebview(); //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效
+				currentWebview.append(wv);//一定要append到当前的页面里！！！才能跟随当前页面一起做动画，一起关闭
+				
+			} else {
+				this.isIOS = false;
+				this.url = option.url;
+			}	
 			
 			this._firstLoad = true;
 		},
@@ -45,6 +66,16 @@
 				path: '/pages/index/index',
 			}
 		},
+		onReady() {
+			// #ifdef APP-PLUS
+			// var currentWebview = this.$scope.$getAppWebview() //获取当前页面的webview对象
+			// setTimeout(function() {
+			// 	var wv = currentWebview.children()[0];
+			// 	// wv.setStyle({scalable:true});
+			// 	console.log("wv", wv);
+			// }, 1000); //如果是页面初始化调用时，需要延时一下
+			// // #endif
+		}
 	}
 </script>
 
